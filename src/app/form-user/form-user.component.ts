b3ac3ae02a991c69dc8a9e31a6c67bc6820d8dbb
morbi <elae.debichi@esprit.tn>
2021-10-21 14:38:24 +0100

    bassem :)

--- src/app/form-user/form-user.component.ts
+++ src/app/form-user/form-user.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {User} from "../model/user";
+import {UserService} from "../services/user.service";
 
 @Component({
   selector: 'app-form-user',
@@ -8,16 +9,18 @@ import {User} from "../model/user";
 })
 export class FormUserComponent implements OnInit {
 user: User;
-list: User[];
-  constructor() { }
+//list: User[];
+  constructor(private userService : UserService) { }
 
   ngOnInit(): void {
     this.user = new User();
-    this.list=[];
+    //this.list=[];
   }
   save(){
-    this.list.push(this.user);
-    console.log(this.list);
+    this.user.accountCategory = 'Customer';
+    this.user.picture = '';
+    this.userService.list.push(this.user);
+    console.log(this.userService.list);
 
   }
 
